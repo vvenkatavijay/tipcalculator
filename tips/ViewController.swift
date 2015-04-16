ab74c30f5ded28bdf66323bf82e7e007b321393d
vvenkatavijay <Vijay@Venkatas-MacBook-Pro.local>
2015-04-16 07:48:10 -0700

    Final app

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -14,12 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipPlusLabel: UILabel!
+    @IBOutlet weak var totalEqualLabel: UILabel!
+    @IBOutlet weak var tipAndTotalView: UIView!
+    
     let defaults = NSUserDefaults.standardUserDefaults()
     let currencySymbolArray = ["$", "£", "€"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //self.view.backgroundColor = UIColor.brownColor()
+       // self.tipAndTotalView.backgroundColor = UIColor.brownColor()
+        billField.font = UIFont (name: "HelveticaNeue-UltraLight", size: 48)
+        billField.textAlignment = .Right
+        
+        if defaults.doubleForKey("valueBillField") != 0 {
+            billField.text = String(format:"%.2f", defaults.doubleForKey("valueBillField"))
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +43,16 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         
         tipControl.selectedSegmentIndex = defaults.integerForKey("defaultTip")
+        println(defaults.doubleForKey("valueBillField"))
+        billField.placeholder = currencySymbolArray[defaults.integerForKey("currencyRow")]
         self.updateTotal()
         billField.becomeFirstResponder()
+        self.animateTipAndTotal(0)
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         self.updateTotal()
+        self.animateTipAndTotal(0.3)
     }
     
     func updateTotal() {
@@ -51,10 +67,38 @@ class ViewController: UIViewController {
         tipLabel.text = currencySymbol + String(format: "%.2f", tip)
         totalLabel.text = currencySymbol + String(format: "%.2f", total)
     }
+    
+    func animateTipAndTotal(transformTime: Double) {
+        if billField.text.isEmpty {
+            if tipAndTotalView.alpha == 1 {
+                UIView.animateWithDuration(transformTime, animations: {
+                    self.tipAndTotalView.alpha = 0
+                    
+                    let myTransform:CGAffineTransform  = CGAffineTransformMake(1.0, 0.0, 0.0, 1.0, 0.0, 200.0)
+                    self.billField.transform = myTransform
+                })
+            }
+        } else {
+            if tipAndTotalView.alpha == 0 {
+                UIView.animateWithDuration(transformTime, animations: {
+                    self.tipAndTotalView.alpha = 1
+                    
+                    let myTransform:CGAffineTransform  = CGAffineTransformMake(1.0, 0.0, 0.0, 1.0, 0.0, 0.0)
+                    self.billField.transform = myTransform
+
+                })
+            }
+            
+        }
+    }
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
         let defaults = NSUserDefaults.standardUserDefaults()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+                defaults.setDouble((self.billField.text as NSString).doubleValue, forKey: "valueBillField")
     }
 }
 
